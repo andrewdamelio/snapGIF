4f739c25ae4eb12af3b24395d52d9dd767b5346b
Andrew D'Amelio <dev@andrewdamel.io>
2014-07-30 17:43:43 -0400

    corrected imgur images not being removed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -46,212 +46,213 @@ app.controller('AppCtrl', ['$scope', '$timeout', 'helperService', '$firebase', '
     }
     else { 
 
-    var ref = new Firebase(FIREBASE_URL+$routeParams.id);
-    var localMediaStream;
-    
-    var userMediaQ = $q.defer();
-    var allUserMediaQ = $q.all([userMediaQ.promise]);
-    
-    var fireQ = $q.defer();
-    var allFireQ = $q.all([fireQ.promise]);
+      var ref = new Firebase(FIREBASE_URL+$routeParams.id);
+      var localMediaStream;
+      
+      var userMediaQ = $q.defer();
+      var allUserMediaQ = $q.all([userMediaQ.promise]);
+      
+      var fireQ = $q.defer();
+      var allFireQ = $q.all([fireQ.promise]);
 
-    $scope.pageLoading = true;
+      $scope.pageLoading = true;
 
-    $scope.camera = helperService;
-    $scope.sources = [];
-    $scope.constraints = {};
-    $scope.gifs = $firebase(ref);
-    
-    $scope.gifs.$on('loaded', function() {
-        if ($scope.gifs.$getIndex().length === 0) {
-          $scope.gifs.$add({
-              url: 'https://imgur.com/h8bGicy.gif',
-              comment: 'Welcome to ' +$routeParams.id + ', I exist.',
-              date: new Date()
-            });
-        }
+      $scope.camera = helperService;
+      $scope.sources = [];
+      $scope.constraints = {};
+      $scope.gifs = $firebase(ref);
+      
+      $scope.gifs.$on('loaded', function() {
+          if ($scope.gifs.$getIndex().length === 0) {
+            $scope.gifs.$add({
+                url: 'https://imgur.com/h8bGicy.gif',
+                comment: 'Welcome to ' +$routeParams.id + ', I exist.',
+                date: new Date()
+              });
+          }
+          fireQ.resolve();
+        });
+
+      if ($scope.gifs.$getIndex().length > 0) {
         fireQ.resolve();
-      });
-
-    if ($scope.gifs.$getIndex().length > 0) {
-      fireQ.resolve();
-    }
-
-    allFireQ.then(function(data) {
-      console.log('firebase loaded');
-      if (navigator.webkitGetUserMedia) {
-        MediaStreamTrack.getSources(gotSources);
       }
-      else {
-        userMediaQ.resolve();
-        //This browser does not support MediaStreamTrack.
-      }
-    }, function() {console.log('ERROR');});
-    
 
-    allUserMediaQ.then(function(data) {
-        console.log('stream loaded');
-        $scope.pageLoading = false;
-        if ($scope.sources.length === 0) {
-          $scope.sources.push({name : 'Camera Front', id:1});
+      allFireQ.then(function(data) {
+        console.log('firebase loaded');
+        if (navigator.webkitGetUserMedia) {
+          MediaStreamTrack.getSources(gotSources);
+        }
+        else {
+          userMediaQ.resolve();
+          //This browser does not support MediaStreamTrack.
         }
       }, function() {console.log('ERROR');});
-    }
+      
 
-    function gotSources(sourceInfos) {
-        $scope.sources = [];
-        $scope.videoSources=0;
-        for (var i = 0; i !== sourceInfos.length; ++i) {
-          var sourceInfo = sourceInfos[i];
-          if (sourceInfo.kind === 'video') {
-            $scope.videoSources+=1;
-            var text = sourceInfo.label || 'camera ' + ( $scope.videoSources);
-            $scope.sources.push({name : text, id:sourceInfo.id});
+      allUserMediaQ.then(function(data) {
+          console.log('stream loaded');
+          $scope.pageLoading = false;
+          if ($scope.sources.length === 0) {
+            $scope.sources.push({name : 'Camera Front', id:1});
           }
-        }
-        userMediaQ.resolve($scope.sources);
+        }, function() {console.log('ERROR');});
       }
 
-    $scope.getUserMedia = function () {
-        $scope.mediaON = true;
-
-        if ( navigator.webkitGetUserMedia) {
-          MediaStreamTrack.getSources(gotSources);
-          $scope.constraints = {};
-          $scope.constraints.video = {
-            optional: [{
-                sourceId: $scope.videoSource.id
-              }]
-            };
+      function gotSources(sourceInfos) {
           $scope.sources = [];
+          $scope.videoSources=0;
+          for (var i = 0; i !== sourceInfos.length; ++i) {
+            var sourceInfo = sourceInfos[i];
+            if (sourceInfo.kind === 'video') {
+              $scope.videoSources+=1;
+              var text = sourceInfo.label || 'camera ' + ( $scope.videoSources);
+              $scope.sources.push({name : text, id:sourceInfo.id});
+            }
+          }
+          userMediaQ.resolve($scope.sources);
         }
-        else {
-          //This browser does not support MediaStreamTrack.
-          $scope.constraints = {};
-          $scope.constraints = {
-            video: true
-          };
+
+      $scope.getUserMedia = function () {
+          $scope.mediaON = true;
+
+          if ( navigator.webkitGetUserMedia) {
+            MediaStreamTrack.getSources(gotSources);
+            $scope.constraints = {};
+            $scope.constraints.video = {
+              optional: [{
+                  sourceId: $scope.videoSource.id
+                }]
+              };
+            $scope.sources = [];
+          }
+          else {
+            //This browser does not support MediaStreamTrack.
+            $scope.constraints = {};
+            $scope.constraints = {
+              video: true
+            };
+          }
+          navigator.getUserMedia($scope.constraints, successCallback, errorCallback);
+        };
+
+      function successCallback(stream) {
+          if (video.mozSrcObject !== undefined) {
+            video.mozSrcObject = stream;
+          }
+          else {
+            video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+          }
+          localMediaStream = stream;
         }
-        navigator.getUserMedia($scope.constraints, successCallback, errorCallback);
-      };
 
-    function successCallback(stream) {
-        if (video.mozSrcObject !== undefined) {
-          video.mozSrcObject = stream;
+      function errorCallback(error) {
+          console.log('navigator.getUserMedia error: ', error);
         }
-        else {
-          video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-        }
-        localMediaStream = stream;
-      }
+      
+      $scope.keypress = function (e) {
+          if (e.keyCode !== 13) {
+            return;
+          }
+          $scope.recordGIF();
+        };
 
-    function errorCallback(error) {
-        console.log('navigator.getUserMedia error: ', error);
-      }
-    
-    $scope.keypress = function (e) {
-        if (e.keyCode !== 13) {
-          return;
-        }
-        $scope.recordGIF();
-      };
+      $scope.stopMedia = function () {
+          $scope.mediaON = false;
+          if (localMediaStream) {
+             localMediaStream.stop();
+          }
+        };
 
-    $scope.stop = function () {
-        $scope.mediaON = false;
-        localMediaStream.stop();
-      };
+      $scope.recordGIF = function () {
+          $scope.camera.recordingOn();
+          $scope.text = $scope.comment;
+          $scope.comment = '';
 
-    $scope.recordGIF = function () {
-        $scope.camera.recordingOn();
-        $scope.text = $scope.comment;
-        $scope.comment = '';
+          var context1 = document.getElementById('canvas1').getContext('2d');
+          var context2 = document.getElementById('canvas2').getContext('2d');
+          var context3 = document.getElementById('canvas3').getContext('2d');
+          var context4 = document.getElementById('canvas4').getContext('2d');
+          var context5 = document.getElementById('canvas5').getContext('2d');
 
-        var context1 = document.getElementById('canvas1').getContext('2d');
-        var context2 = document.getElementById('canvas2').getContext('2d');
-        var context3 = document.getElementById('canvas3').getContext('2d');
-        var context4 = document.getElementById('canvas4').getContext('2d');
-        var context5 = document.getElementById('canvas5').getContext('2d');
+          $('pie').removeClass('ten');
+          $('pie').removeClass('twentyfive');
+          $('pie').removeClass('fifty');
+          $('pie').removeClass('seventyfive');
+          $('pie').removeClass('onehundred');
 
-        $('pie').removeClass('ten');
-        $('pie').removeClass('twentyfive');
-        $('pie').removeClass('fifty');
-        $('pie').removeClass('seventyfive');
-        $('pie').removeClass('onehundred');
+          var encoder = new GIFEncoder();
 
-        var encoder = new GIFEncoder();
+          encoder.setRepeat(0);
+          encoder.setDelay(250);
+          encoder.start();
 
-        encoder.setRepeat(0);
-        encoder.setDelay(250);
-        encoder.start();
+          if ($scope.camera.isRecording()) {
+            $('pie').addClass('ten');
+            $timeout(function () {
+              $('pie').addClass('twentyfive');
+              var frame1 = video;
+              context1.drawImage(frame1, 0, 0, 135, 100);
+              encoder.addFrame(context1);
+            }, 500);
+            $timeout(function () {
+              $('pie').addClass('fifty');
+              var frame2 = video;
+              context2.drawImage(frame2, 0, 0, 135, 100);
+              encoder.addFrame(context2);
+            }, 1000);
+            $timeout(function () {
+              $('pie').addClass('seventyfive');
+              var frame3 = video;
+              context3.drawImage(frame3, 0, 0, 135, 100);
+              encoder.addFrame(context3);
+            }, 1500);
+            $timeout(function () {
+              $('pie').addClass('onehundred');
+              var frame4 = video;
+              context4.drawImage(frame4, 0, 0, 135, 100);
+              encoder.addFrame(context4);
+            }, 2000);
+            $timeout(function () {
+              $scope.camera.recordingOff();
+              var frame5 = video;
+              context5.drawImage(frame5, 0, 0, 135, 100);
+              encoder.addFrame(context5);
+              encoder.finish();
 
-        if ($scope.camera.isRecording()) {
-          $('pie').addClass('ten');
-          $timeout(function () {
-            $('pie').addClass('twentyfive');
-            var frame1 = video;
-            context1.drawImage(frame1, 0, 0, 135, 100);
-            encoder.addFrame(context1);
-          }, 500);
-          $timeout(function () {
-            $('pie').addClass('fifty');
-            var frame2 = video;
-            context2.drawImage(frame2, 0, 0, 135, 100);
-            encoder.addFrame(context2);
-          }, 1000);
-          $timeout(function () {
-            $('pie').addClass('seventyfive');
-            var frame3 = video;
-            context3.drawImage(frame3, 0, 0, 135, 100);
-            encoder.addFrame(context3);
-          }, 1500);
-          $timeout(function () {
-            $('pie').addClass('onehundred');
-            var frame4 = video;
-            context4.drawImage(frame4, 0, 0, 135, 100);
-            encoder.addFrame(context4);
-          }, 2000);
-          $timeout(function () {
-            $scope.camera.recordingOff();
-            var frame5 = video;
-            context5.drawImage(frame5, 0, 0, 135, 100);
-            encoder.addFrame(context5);
-            encoder.finish();
+              var binaryGif = encoder.stream().getData();
+              var data = 'data:image/gif;base64,' + encode64(binaryGif);
+              var keys = $scope.gifs.$getIndex();
 
-            var binaryGif = encoder.stream().getData();
-            var data = 'data:image/gif;base64,' + encode64(binaryGif);
 
-            $.ajax({
-                url: 'https://api.imgur.com/3/upload',
-                type: 'POST',
-                datatype: 'base64',
-                data: encode64(binaryGif),
-                success: function (result) {
-                    var id = result.data.id;
-                    var imgsrc = 'https://imgur.com/' + id + '.gif';
-                    var keys = $scope.gifs.$getIndex();
+              
+              helperService.addGIF(keys, encode64(binaryGif)).then(function (result) {
+                  console.log("added");
+                  console.log(result);
+                  var id = result.data.id;
+                      var imgsrc = 'https://imgur.com/' + id + '.gif';
+                      
+                      if (keys.length > 30) {
+                          helperService.deleteGIF($scope.gifs[keys[0]].deleteHash);
+                          $scope.gifs.$remove(keys[0]);
+                      }
 
-                    $scope.gifs.$add({
-                        url: imgsrc,
-                        comment: $scope.text,
-                        date: new Date()
-                      });
-                    if (keys.length > 30) {
-                      $scope.gifs.$remove(keys[0]);
-                    }
-                  },
-                error: function (err) {
-                    console.log(err);
-                  },
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader('Authorization', IMGUR_API_KEY);
-                  }
-              });
-          }, 2500);
-        }
+                      $scope.gifs.$add({
+                          url: imgsrc,
+                          comment: $scope.text,
+                          date: new Date(),
+                          deleteHash : result.data.deletehash
+                        });              
+
+              }, function (reason) {
+                  console.log(reason);
+              });              
+
+            }, 2500);
+          }
       };
   }]);
 
-app.factory('helperService', function () {
+app.factory('helperService', function (IMGUR_API_KEY, $q) {
     var recording = false;
 
     var isRecording = function () {
@@ -266,10 +267,54 @@ app.factory('helperService', function () {
         recording = false;
       };
 
+
+    var deleteGIF = function(hash) {
+      if (hash) {
+          $.ajax({
+            url: 'https://api.imgur.com/3/image/'+hash,
+            type: 'DELETE',
+                success: function (result) {},
+                error: function (err) {
+                    console.log(err);
+                },
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', IMGUR_API_KEY);
+                }
+          });      
+      }
+    };
+
+    var addGIF = function(keys, data) {
+  
+        var deferredAdd = $q.defer();
+        $.ajax({
+                  url: 'https://api.imgur.com/3/upload',
+                  type: 'POST',
+                  datatype: 'base64',
+                  data: data,
+                  success: function (result) {
+                    console.log(result);
+                        deferredAdd.resolve(result);
+                    },
+                  error: function (err) {
+                      console.log(err);
+                    },
+                  beforeSend: function (xhr) {
+                      xhr.setRequestHeader('Authorization', IMGUR_API_KEY);
+                    }
+        });
+
+        return deferredAdd.promise;
+
+    }
+
     return {
         isRecording: isRecording,
         recordingOn: recordingOn,
-        recordingOff: recordingOff
+        recordingOff: recordingOff,
+        deleteGIF: deleteGIF,
+        addGIF: addGIF
+
       };
   });
 
@@ -285,6 +330,6 @@ app.directive('github', [function () {
   return {
     restrict: 'E',
     replace: true,
-    template: "<a href='https://github.com/andrewdamelio/chat.gif'><svg class='github' version='1.1' id='Layer_2' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='15.835px' height='20.164px' viewBox='242.137 3.418 15.835 18.164' enable-background='new 242.137 3.418 15.835 18.164' xml:space='preserve'>                <path fill='#333' stroke='#333' stroke-width='1' stroke-miterlimit='10' d='M256.255,3.943c0,0-0.904-0.292-2.967,1.107c-0.864-0.239-1.787-0.359-2.704-0.363c-0.919,0.004-1.843,0.124-2.705,0.363c-2.063-1.398-2.97-1.107-2.97-1.107c-0.587,1.486-0.217,2.585-0.106,2.858c-0.691,0.755-1.112,1.719-1.112,2.898c0,4.14,2.522,5.066,4.92,5.339c-0.309,0.271-0.587,0.747-0.686,1.445c-0.616,0.276-2.18,0.752-3.144-0.897c0,0-0.57-1.038-1.655-1.114c0,0-1.055-0.013-0.074,0.657c0,0,0.708,0.332,1.199,1.58c0,0,0.634,2.101,3.638,1.448c0.006,0.901,0.016,1.581,0.016,1.838c0,0.281,0-0.266-0.021,0.541c0.193,0.675,1.512,0.531,2.698,0.531c1.187,0,2.291,0.167,2.65-0.551c0.14-0.686,0.029-0.238,0.029-0.521c0-0.355,0.011-1.52,0.011-2.964c0-1.008-0.345-1.667-0.733-2c2.406-0.268,4.933-1.181,4.933-5.331c0-1.179-0.419-2.143-1.109-2.898C256.476,6.528,256.847,5.429,256.255,3.943z'></path></svg></a"
+    template: "<a href='https://github.com/andrewdamelio/snapgif'><svg class='github' version='1.1' id='Layer_2' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='15.835px' height='20.164px' viewBox='242.137 3.418 15.835 18.164' enable-background='new 242.137 3.418 15.835 18.164' xml:space='preserve'>                <path fill='#333' stroke='#333' stroke-width='1' stroke-miterlimit='10' d='M256.255,3.943c0,0-0.904-0.292-2.967,1.107c-0.864-0.239-1.787-0.359-2.704-0.363c-0.919,0.004-1.843,0.124-2.705,0.363c-2.063-1.398-2.97-1.107-2.97-1.107c-0.587,1.486-0.217,2.585-0.106,2.858c-0.691,0.755-1.112,1.719-1.112,2.898c0,4.14,2.522,5.066,4.92,5.339c-0.309,0.271-0.587,0.747-0.686,1.445c-0.616,0.276-2.18,0.752-3.144-0.897c0,0-0.57-1.038-1.655-1.114c0,0-1.055-0.013-0.074,0.657c0,0,0.708,0.332,1.199,1.58c0,0,0.634,2.101,3.638,1.448c0.006,0.901,0.016,1.581,0.016,1.838c0,0.281,0-0.266-0.021,0.541c0.193,0.675,1.512,0.531,2.698,0.531c1.187,0,2.291,0.167,2.65-0.551c0.14-0.686,0.029-0.238,0.029-0.521c0-0.355,0.011-1.52,0.011-2.964c0-1.008-0.345-1.667-0.733-2c2.406-0.268,4.933-1.181,4.933-5.331c0-1.179-0.419-2.143-1.109-2.898C256.476,6.528,256.847,5.429,256.255,3.943z'></path></svg></a"
   };
 }]);
